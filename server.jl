d8178b72d2c2e0d8fc647de1a1e10676ff11c51a
Jarno Leppänen <jarno.leppanen@iki.fi>
2015-08-04 17:24:47 +0300

    Add opus

--- server.jl
+++ server.jl
@@ -1,6 +1,11 @@
+include("opus.jl")
+
 a = UdpSocket()
 port = 53354
 ip = ip"0.0.0.0"
+fs = 48000
+channels = 1
+enc = Opus.encoder_create(fs, channels)
 try
     if bind(a,ip,port)
         data = recv(a)
@@ -9,4 +14,5 @@ try
 catch
     # catch Ctrl-C
 end
+Opus.encoder_destroy(enc)
 close(a)
